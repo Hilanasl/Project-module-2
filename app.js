401f3e59a2a5c465e68112985d87a7b4b9af1090
Hilana <hilana.sene@gmail.com>
2022-01-17 14:41:48 +0100

    navbar

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ const app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+hbs.registerPartials(path.join(__dirname, "views/partials"));
 
 app.use(logger('dev'));
 app.use(express.json());
