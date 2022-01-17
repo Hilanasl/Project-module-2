018a9a6eb252461e75fb52f040eafc332bd56bbf
Hilana <hilana.sene@gmail.com>
2022-01-17 14:33:52 +0100

    commit

--- models/CardModel.js
+++ models/CardModel.js
@@ -4,14 +4,14 @@ const Schema = mongoose.Schema;
 
 const cardSchema = new Schema(
     {
-    title: String,
-    image: req.file.url,
-    category: { type: String, enum: ['museum', 'exhibition', 'street-art', 'public-space'] },
-    location: String,
-    address: String,
-    arrond: { type: String, enum: ['1st', '2nd', '3rd', '4th', '5th', '6th', '7th', '8th', '9th', '10th', '11th', '12th', '13th', '14th', '15th', '16th', '17th', '18th', '19th', '20th'] },
-    websiteUrl: String,
-    comments: String
+        title: String,
+        image: String,
+        category: { type: String, enum: ['museum', 'exhibition', 'street-art', 'public-space'] },
+        location: String,
+        address: String,
+        arrond: { type: String, enum: ['1st', '2nd', '3rd', '4th', '5th', '6th', '7th', '8th', '9th', '10th', '11th', '12th', '13th', '14th', '15th', '16th', '17th', '18th', '19th', '20th'] },
+        websiteUrl: String,
+        comments: String
     }
 );
 
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const Card = require('./../models/CardModel')
+const User = require('./../models/UserModel')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
 
 module.exports = router;
