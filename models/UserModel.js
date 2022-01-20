c442143a219db31c91ae164deba46c414274240e
Anna Baldwin <annabaldwin56@gmail.com>
2022-01-20 18:24:22 +0100

    favourites finished

--- models/UserModel.js
+++ models/UserModel.js
@@ -4,19 +4,11 @@ const Schema = mongoose.Schema;
 
 const userSchema = new Schema(
     {
-<<<<<<< HEAD
-        fullName: String,
-        username: String,
-        email: { type: String, unique: true },
-        password: String,
-        favourites: [{ type: Schema.Types.ObjectId, ref: "cards" }]
-=======
     fullName: String,
     username: String,
     email: {type: String, unique: true},
     password: String,
     favourites: [{type: Schema.Types.ObjectId, ref: 'cards'}]
->>>>>>> fdbaaa3948fe69016e2f7c6defd5ece868c8af77
     }
 );
 
